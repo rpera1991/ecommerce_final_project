fd585048ab58a2f7645a101e6979d27d413e1920
rpera1991 <66051954+rpera1991@users.noreply.github.com>
2020-09-20 15:28:04 -0300

    Update(entrega 4)
    a lot of things

--- js/product-info.js
+++ js/product-info.js
@@ -5,18 +5,54 @@ function showImagesGallery(array) {
 
     let htmlContentToAppend = "";
 
-    for (let i = 0; i < array.length; i++) {
-        let imageSrc = array[i];
-        htmlContentToAppend += `
-        <div class="col-lg-3 col-md-4 col-6 contenedor">
-            <div class="d-block mb-4 h-100">
-            <a href="${imageSrc}" target="_blank"><img class="img-fluid img-thumbnail imagen" src="${imageSrc}" alt=""></a>
-            </div>
-        </div>
-        `
-        document.getElementById("productImagGallery").innerHTML = htmlContentToAppend;
-    }
+    htmlContentToAppend += `
+              <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+          <img class="d-block w-100" src="${array[0]}" alt="First slide">
+          </div>
+          <div class="carousel-item">
+          <img class="d-block w-100" src="${array[1]}" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+          <img class="d-block w-100" src="${array[2]}" alt="Third slide">
+          </div>
+          <div class="carousel-item">
+          <img class="d-block w-100" src="${array[3]}" alt="Four slide">
+          </div>
+          <div class="carousel-item">
+          <img class="d-block w-100" src="${array[4]}" alt="Five slide">
+          </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+      </a>
+      </div>
+      `
+    document.getElementById("productImagGallery").innerHTML = htmlContentToAppend;
 }
+
+// function showImagesGallery(array) {
+
+//     let htmlContentToAppend = "";
+
+//     for (let i = 0; i < array.length; i++) {
+//         let imageSrc = array[i];
+//         htmlContentToAppend += `
+//         <div class="col-lg-3 col-md-4 col-6 contenedor">
+//             <div class="d-block mb-4 h-100">
+//                  <a href="${imageSrc[]}" target="_blank"><img class="img-fluid img-thumbnail imagen" src="${imageSrc}" alt=""></a>
+//             </div>
+//         </div>
+//         `
+//         document.getElementById("productImagGallery").innerHTML = htmlContentToAppend;
+//     }
+// }
 
 function showRelatedProduct(array) {
 
